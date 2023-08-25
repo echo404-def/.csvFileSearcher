dc9f598ec18acb834127290727cc683f52b6a64f
MHP <85354497+echo404-def@users.noreply.github.com>
2023-08-26 00:11:46 +0900

    Add files via upload

--- csvFileSearcher.py
+++ csvFileSearcher.py
@@ -22,6 +22,7 @@ else:
     print("\aLimit exceeded.")
             
 
+
 def check_word_in_text(partial_word, text):
     pattern = re.compile(re.escape(partial_word), re.IGNORECASE)
     match = pattern.search(text)
@@ -30,14 +31,55 @@ def check_word_in_text(partial_word, text):
     else:
         return False
 
-print("To exit, type 'exit'")
-
-while True:
-    inp = input("word >")
-    if inp == "exit":
-        break
-    elif inp != "":
+def search(word):
+    res = []
+    if word != "":
         for i in data:
             for i2 in i:
-                if check_word_in_text(inp, i2):
-                    print(i)
+                if check_word_in_text(word, i2):
+                    res.append(i)
+        return(res)
+    
+
+def pressed(event=None):
+    word = entry_1.get()
+    ls = search(word)
+    res = ""
+    n = len(ls)
+    for i in ls:
+        for i2 in i:
+            res += i2 + " / "
+        res += "\n"*2
+    textbox.delete("1.0",tk.END)
+    textbox.insert(tk.END,res)
+    label_1["text"] = f"{n}件ヒット"
+
+
+try:
+    import tkinter as tk
+    root = tk.Tk()
+    root.title("蔵書検索")
+    frame_1 = tk.Frame(root)
+    frame_1.pack()
+    label_1 = tk.Label(frame_1,text="")
+    label_1.grid(column=0,row=0)
+    entry_1 = tk.Entry(frame_1)
+    entry_1.grid(column=1,row=0)
+    btn_1 = tk.Button(frame_1,text="検索",command=pressed)
+    btn_1.grid(column=2,row=0)
+    textbox = tk.Text(root)
+    textbox.pack()
+    
+    entry_1.bind("<Return>",pressed)
+    
+    root.mainloop()
+    
+except:
+    print("To exit, type 'exit'")
+    
+    while True:
+        inp = input("word >")
+        if inp == "exit":
+            break
+        else:
+            print(search(inp))
